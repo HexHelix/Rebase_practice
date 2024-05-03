ecb9cdff33aa7847518ad6b5a6a1e8763e0f6fe3
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-05-03 14:11:29 +0530

    [Priyanshu] Mod: Modified Hates Controller, renamed GReetController as HatesController

--- PracticeApi/Controllers/HatesController.cs
+++ PracticeApi/Controllers/HatesController.cs
@@ -5,7 +5,7 @@ namespace PracticeApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GreetController : ControllerBase
+    public class HatesController : ControllerBase
     {
     }
 }
